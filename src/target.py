1f28106fe73f92bb1c6b3f7920f17be9c0c17006
tcp-22 <60749913+tcp-22@users.noreply.github.com>
2023-08-29 23:12:29 +0100

    Update target.py

--- src/target.py
+++ src/target.py
@@ -14,18 +14,19 @@ class TargetStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, pipeline, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
     
-    target_sg = ec2.SecurityGroup(self, "sec-group",security_group_name="sg_aaos_target", 
+    target_sg = ec2.SecurityGroup(self, "sec-group",security_group_name="sg_aaos_targett", 
       vpc=pipeline.vpc, 
       allow_all_outbound=True)
     
-    
+  
     # Allow SSH (TCP port 22) and 8443 traffic
-    target_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), 'Allow SSH traffic');
-    target_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(8443), 'Allow webUI traffic');
-
+    target_sg.add_ingress_rule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(22), 'Allow SSH traffic');
+    target_sg.add_ingress_rule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(8443), 'Allow webUI traffic');
+    target_sg.add_ingress_rule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(6520), 'ADB for Android Studio or shell');
+    target_sg.add_ingress_rule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(6444), 'VNC -- if using');
     # Allow TCP and UDP traffic from port 15550 to port 15599
-    target_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp_range(15550, 15599), 'Allow webrtc tcp traffic');
-    target_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.udp_range(15550, 15599), 'Allow webrtc udp traffic');
+    target_sg.add_ingress_rule(ec2.Peer.ipv4('51.186.0.195/0'), ec2.Port.tcp_range(15550, 15599), 'Allow webrtc tcp traffic');
+    target_sg.add_ingress_rule(ec2.Peer.ipv4('51.186.0.195/0'), ec2.Port.udp_range(15550, 15599), 'Allow webrtc udp traffic');
     
     role = iam.Role(self, 'ec2Role',
       assumed_by=iam.ServicePrincipal('ec2.amazonaws.com'),
@@ -40,48 +41,57 @@ class TargetStack(Stack):
     
     user_data = ec2.UserData.for_linux()
     user_data.add_commands(
-      "apt-get update -y",
-      "apt-get install -y git devscripts config-package-dev debhelper-compat golang nfs-common binutils u-boot-tools",
+      "sudo sed -i \"/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/\" /etc/needrestart/needrestart.conf",
+      "sudo apt-get update -y",
+      "sudo apt-get install -y libprotobuf-dev protobuf-compilerapt-get nfs-common binutils u-boot-tools",
+      "sudo apt-get install -y git devscripts config-package-dev debhelper-compat golang",
+      "sudo apt-get -y install awscli",
+      "sudo apt-get -y install unzip",
       "cd ~",
+      "cd /home/ubuntu ",
       "git clone https://github.com/google/android-cuttlefish",
-      "cd android-cuttlefish",
+      "cd android-cuttlefish ",
+      "sudo apt-get install libprotobuf-dev protobuf-compiler -y",
+      "sudo apt-get install dpkg-dev -y",
       "for dir in base frontend; do",
       "  pushd $dir",
       "  dpkg-buildpackage -uc -us",
       "  popd",
       "done",
-      "apt install -y ./cuttlefish-base_*.deb ./cuttlefish-user_*.deb",
-      "usermod -aG kvm,cvdnetwork,render ubuntu",
-      "cd ~",
-      "curl https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip -o awscliv2.zip",
-      "unzip awscliv2.zip",
-      "./aws/install",
-      "mkdir /stage/",
-      "cd /stage/",
+      "sudo apt install -y ./cuttlefish-base_*.deb",
+      "sudo apt install -y ./cuttlefish-user_*.deb",
+      "sudo usermod -aG kvm,cvdnetwork,render ubuntu",
+      "cd /home/ubuntu ",
+      "mkdir /home/ubuntu/stage ",
+      "cd /home/ubuntu/stage ",
       "aws s3 cp s3://{}/cvd-host_package.tar.gz .".format(pipeline.bucket.bucket_name),
       "aws s3 cp s3://{}/images.zip .".format(pipeline.bucket.bucket_name),
       "tar xvf ./cvd-host_package.tar.gz",
       "unzip ./images.zip",
       "aws s3 cp s3://{}/u-boot.bin ./bootloader".format(pipeline.bucket.bucket_name),
-      "cp /usr/bin/mkenvimage ./bin/mkenvimage",
-      "chown -R ubuntu:ubuntu /stage",
-      "cat <<EOF >>/etc/systemd/system/cvd.service",
+      "sudo cp /usr/bin/mkenvimage ./bin/mkenvimage",
+      "sudo chown -R ubuntu:ubuntu /home/ubuntu/stage",
+      "cat <<EOF >> /etc/systemd/system/cvd.service",
       "[Unit]",
       "Description=Cuttlefish Virtual Device",
       "After=multi-user.target",
       "[Service]",
-      "Environment=\"HOME=/stage\"",
+      "Environment='HOME=/home/ubuntu/stage'",
       "Type=simple",
       "User=ubuntu",
       "Group=ubuntu",
-      "ExecStart=/bin/sh -c \'yes Y | /stage/bin/launch_cvd\'",
-      "ExecStop=/stage/bin/stop_cvd",
+      "ExecStart=/bin/sh -c 'yes Y | /home/ubuntu/stage/bin/launch_cvd'",
+      "ExecStop=/home/ubuntu/stage/bin/stop_cvd",
       "[Install]",
       "WantedBy=multi-user.target",
       "EOF",
+      "sudo chmod 644 /etc/systemd/system/cvd.service",
       "systemctl daemon-reload",
       "systemctl enable cvd.service",
-      "reboot")
+      "systemctl restart cvd.service",
+      "systemctl status cvd.service",
+      "sudo reboot"
+  )
     
     # Key pair to access to target
     key = KeyPair(self, "MyKey",
